570cbd0ea428e8f4035ed50067d17a65b436cc8b
Jaybert Bajenting <jaybertbajenting99@gmail.com>
2024-04-02 14:39:54 +0800

    hehe

---
+++ src/test/java/com/capstone/EventEase/EventEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.EventEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
