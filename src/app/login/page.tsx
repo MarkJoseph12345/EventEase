62d78ccb191ad84e655afc794269e22d835cf16e
MargauxKylie <forschoolpurposesonly4@gmail.com>
2024-04-18 01:53:16 +0800

    18

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -51,42 +51,42 @@ const Login = () => {
 
     return (
         <div className="max-w-[2000px] bg-cover bg-no-repeat bg-center bg-[url('/BG.png')] h-screen box-border px-[5%] py-[5%] flex justify-end mx-auto">
-            <div className="flex justify-center w-[40%] h-full bg-black box-border p-10 rounded-2xl">
+            <div className="flex justify-center w-[38%] h-full bg-black box-border p-10 rounded-2xl">
                 <form onSubmit={handleLoginSubmit} method="post" className="flex flex-col items-center w-full h-full bg-customYellow rounded-2xl box-border justify-between	py-10">
                     <div className="flex flex-col items-center">
-                        <h1 className="text-5xl font-bold">WELCOME!</h1>
-                        <p>Please enter your Login and Password</p>
+                        <h1 className="text-4xl font-extrabold font-poppins -mt-7">WELCOME!</h1>
+                        <p className="text-xs font-light font-poppins mb-2">Please enter your Login and Password</p>
                     </div>
-                    <div className="w-[60%]">
-                        <p>Username/Email Address<span className="text-red-800">*</span></p>
+                    <div className="w-[63%] -mt-1">
+                        <p className="font-poppins text-sm font-regular">Username/Email Address<span className="text-red-800">*</span></p>
                         <input
                             type="text"
                             name="username"
                             value={formData.username}
                             onChange={handleInputChange}
                             placeholder="Username/Email Address"
-                            className="w-full h-[50px] rounded-2xl border-4 border-black" />
+                            className="w-full h-[37px] rounded-2xl border-2 border-black" />
                     </div>
-                    <div className="w-[60%]">
-                        <p>Password<span className="text-red-800">*</span></p>
+                    <div className="w-[63%] -mt-1">
+                        <p className="font-poppins text-sm font-regular">Password<span className="text-red-800">*</span></p>
                         <div className="relative box-border">
                             <input type={showLoginPassword ? "text" : "password"}
                                 name="password"
                                 value={formData.password}
                                 onChange={handleInputChange}
-                                placeholder="Password"
-                                className="pr-8 w-full rounded-2xl h-[50px] border-4 border-black" />
+                                placeholder="Enter Password"
+                                className="pr-8 w-full rounded-2xl h-[37px] border-2 border-black" />
                             <span className="absolute inset-y-0 right-0 flex items-center pr-1">
-                                <span className="cursor-pointer" onClick={handleClickShowPassword}>
+                                <span className="cursor-pointer -ml-7 -mt-.5" onClick={handleClickShowPassword}>
                                     {showLoginPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
                                 </span>
                             </span>
                         </div>
-                        <span className="flex justify-end">Forgot Password?</span>
+                        <span className="text-xs flex justify-end font-light mb-2 mt-.5">Forgot Password?</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <button type="submit" className="bg-black text-customYellow w-[200px] h-[50px] text-2xl rounded font-bold">LOGIN</button>
-                        <p>Don't have an account? <Link href="/signup" replace className="font-bold cursor-pointer">SIGN IN</Link></p>
+                        <button type="submit" className="bg-black text-customYellow w-[110px] h-[35px] text-xl rounded font-bold mb-5">LOGIN</button>
+                        <p className="font-light text-xs font-poppins -mt-5">Don't have an account? <Link href="/signup" replace className="font-bold cursor-pointer mt-5">SIGN IN</Link></p>
                     </div>
                 </form>
             </div>
